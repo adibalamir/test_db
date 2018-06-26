ae0c2e9246be6b671c719f8a369803e43a9cd796
Adib <adibalamir@hotmail.com>
2018-06-26 17:09:15 +0000

    function query firstname/lastname

---
+++ lookup_people.js
@@ -0,0 +1,36 @@
+const getName = process.argv[2];
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+function searchName() {
+  const queryString = `SELECT * FROM famous_people WHERE first_name = '${getName}' OR last_name = '${getName}'`
+  console.log("Searching...")
+  client.connect((err) => {
+    if (err) {
+      return console.error("Connection Error", err);
+    }
+    client.query(queryString, (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      console.log(`Found ${result.rows.length} person(s) by the name '${getName}':`)
+      let id = 0
+      for (let i = 0; i < result.rows.length; i++) {
+        id += 1
+        console.log("- " + id + ": " + result.rows[i].first_name + " " + result.rows[i].last_name + " born " + result.rows[i].birthdate)
+      }
+      client.end();
+    });
+  });
+}
+
+searchName();
